4868ca4e68366a29a093735faf50448ee85204da
Jeff <zombitroid@gmail.com>
2021-10-20 17:37:27 -0700

    Formatting changes

--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@ main(void) {
   // Set CS00 and CS02 bits in TCCR0B
   TCCR0B = (1 << CS00) | (1 << CS02);
   // TCCR0B = TCCR0B | ((1 << 3) | (1 << 4));
-  TIFR0 = 0x01;
+
   while (1) {
     // DDRA = 0xff;
-    while ((TIFR0 && 0x01) == 0) { DDRA = 0xff; }
+    while ((TIFR0 && 0x01) == 0);
     // PORTA ^= (0x01 << LED);
     TCNT0 = 0x00;
-    TIFR0 = 0x01; //clear timer0 overflow flag
+    // Clear timer0 overflow flag
+    TIFR0 = 0x01;
     timerOverflowCount++;
     if (timerOverflowCount >= 60) {
       PORTA ^= (0x01 << LED);
@@ -42,9 +43,9 @@ main(void) {
   configure LED pin as output 
   DDRA |= 1<<DDRA5;
   while(1) {
-    check the button status (press - 0 , release - 1 ) 
+    check the button status (press - 0 , release - 1 )
     if(!(PINB & (1<<PINB1))) {
-      switch on the LED until button is pressed 
+      switch on the LED until button is pressed
       PORTA &= ~(1<<PORTA5);
     }
     else {
